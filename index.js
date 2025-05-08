9e4c519a63033065d63e8dde7ec8630d062bd94e
Atharav31 <atharavuttekar@gmail.com>
2025-05-08 19:38:14 +0530

    small update in allowed origin

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
 	"http://localhost:5173",
-	"https://quick-collab-frontend.vercel.app/",
+	"https://quick-collab-frontend.vercel.app",
 	// "*",
 ];
 
